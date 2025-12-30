4494972126f5653c2e7ec9d73a96762009cfb320
Itz_jeslin <124077022+jeslin612@users.noreply.github.com>
2025-12-30 14:35:00 +0530

    Create BreakandContinue.java

---
+++ BreakandContinue.java
@@ -0,0 +1,26 @@
+public class BreakandContinue {
+	public static void main(String[] args) {
+		
+		System.out.println("break:");
+		for ( int i = 1 ; i <= 10; i++) {
+			
+			if (i == 5) {
+				break;
+			}
+			
+			System.out.println(i);
+		}
+		
+		System.out.println("continue:");
+		for (int j = 1 ; j <= 10 ; j++) {
+			
+			if (j == 5) {
+				continue;
+			}
+			
+			System.out.println(j);
+		}
+			
+	}
+
+}
